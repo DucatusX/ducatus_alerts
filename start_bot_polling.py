1bbff0976b350ec704f350ca3fcbaa21c2cd7a26
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2024-01-23 19:01:47 +0300

    refactor

--- start_bot_polling.py
+++ start_bot_polling.py
@@ -5,5 +5,6 @@ from src.settings import config
 
 if __name__ == "__main__":
     for network_name, network_config in config.NETWORKS.items():
+        print(f"Launch bot on {network_name}")
         bot = Bot(network_name, network_config)
         bot.start()
