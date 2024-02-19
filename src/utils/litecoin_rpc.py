bd1b9dfcb0a587b67cf600d43bd18602238885af
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2024-02-19 19:21:15 +0300

    add retry on http exception

--- src/utils/litecoin_rpc.py
+++ src/utils/litecoin_rpc.py
@@ -1,9 +1,32 @@
-from bitcoinrpc.authproxy import AuthServiceProxy
-from src.settings import config, NetworkSettings
+import logging
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from http.client import RemoteDisconnected
+from socket import timeout
 
+from src.settings import config, NetworkSettings
 
 class DucatuscoreInterfaceException(Exception):
     pass
+
+def retry_on_http_disconnection(req):
+    def wrapper(*args, **kwargs):
+        for attempt in range(10):
+            logging.info(f"attempt:{attempt}")
+            try:
+                return req(*args, **kwargs)
+            except RemoteDisconnected as e:
+                logging.warning(e)
+                rpc_response = False
+            except (timeout, TimeoutError) as e:
+                logging.warning(e)
+                rpc_response = False
+            if not isinstance(rpc_response, bool):
+                logging.debug(rpc_response)
+                break
+        else:
+            raise DucatuscoreInterfaceException("cannot get unspent with 10 attempts")
+
+    return wrapper
 
 
 class DucatuscoreInterface:
@@ -22,9 +45,14 @@ class DucatuscoreInterface:
         )
         return
 
+    @retry_on_http_disconnection
     def check_connection(self):
         block = self.rpc.getblockcount()
         if block and block > 0:
             return True
         else:
             raise DucatuscoreInterfaceException('Ducatus node not connected')
+
+    @retry_on_http_disconnection
+    def get_wallet_balance(self) -> float:
+        return self.rpc.getbalance('')
